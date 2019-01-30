f9f8544d9f3abf9d39eaf37c7b7ca8e6e9b7729d
VonBergen-Work <AVonBergen@billingsclinic.org>
2019-01-30 16:07:18 -0700

    clean up

---
+++ Codecademy.py
@@ -0,0 +1,38 @@
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+attractions = [[] for destination in destinations]
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
+
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+  except ValueError:
+    print('No Such Destination')
+    return
+  attractions[destination_index].append(attraction)
+
+test_destination_index = get_traveler_location(test_traveller)
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(test_destination_index)
+print(attractions)
